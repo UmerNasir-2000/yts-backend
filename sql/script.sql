a84536b5698b0e06c26ac7325bd56249c8d4a108
Umer Nasir <unasir@dawaai.pk>
2022-09-05 18:03:47 +0500

    Updated SQL Script. Boolean Column

--- sql/script.sql
+++ sql/script.sql
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 CREATE TABLE IF NOT EXISTS `users_favourites` (
     `id` INT AUTO_INCREMENT,
-    `is_notified` boolean default false,
+    `is_notified` BOOLEAN default false,
     `user_id` INT NOT NULL,
      FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON UPDATE CASCADE ON DELETE NO ACTION,
      PRIMARY KEY (`id`)
